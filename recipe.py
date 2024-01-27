9d4c94cbb974d25cfc1129132b7358d743917415
bpalanka <bela.palankar@gmail.com>
2024-01-26 23:40:29 -0500

    changed

--- recipe.py
+++ recipe.py
@@ -2,10 +2,4 @@
 import pandas as pd
 import taipy
 
-import csv
-print ("hello world")
 
-with open("./ingredients.csv", 'r') as file:
-  csvread = csv.reader(file)
-  for r in csvread:
-    print(r)
