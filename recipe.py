dd1f335a95df70d79f75189e12eb1f9668717e8e
bpalanka <bela.palankar@gmail.com>
2024-01-27 12:25:56 -0500

    For loop to iterate

--- recipe.py
+++ recipe.py
@@ -19,9 +19,29 @@ try:
     # Read the CSV file into a DataFrame
     df = pd.read_csv(csv_file_path)
     print(df)
+
+    #*******************************CHANGES MADE STARTING FROM HERE 
+    #testing with chicken as user input
+    userlist = ['chicken']
+    userlist.append('butter')
+
+
+    #for loop to iterate through the ingredient list
+    ingredientList = ['1 (3Â½â€“4-lb.) whole chicken', '2Â¾ tsp. kosher salt', 'divided, plus more', '2 small acorn squash (about 3 lb. total)', '2 Tbsp. finely chopped sage', '1 Tbsp. finely chopped rosemary', '6 Tbsp. unsalted butter']
+    print(len(ingredientList))
+
+    print(userlist[1])
+    #two for loops to iterate through both lists + check if ingredient list has all of the items in userlist
+    for i in range(len(userlist)):
+            for j in range(len(ingredientList)):
+                if userlist[i] in ingredientList[j]:
+                    print("YAY")
+            
+
 except pd.errors.EmptyDataError:
     print("The CSV file is empty.")
 except FileNotFoundError:
     print(f"File not found: {csv_file_path}")
 except Exception as e:
     print(f"An error occurred: {e}")
+
