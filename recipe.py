7f2b6f650ea2c0044e502540d3e83e49319b7385
bpalanka <bela.palankar@gmail.com>
2024-01-27 16:20:33 -0500

    walmart ctrl f

--- recipe.py
+++ recipe.py
@@ -21,18 +21,58 @@ try:
     allIngredients = df["Ingredients"].str.split(",") # pandas uses str.split() rather than the normal .split() for strings. this command splits the ingredient column by comas. 
     print(allIngredients)
 
-    for index, ingredients_list in allIngredients.items() : # index = 0, ingredients_list in 
-        print(f"Recipe {index} ingredients:")
-        for ingredient in ingredients_list:
-            print(ingredient.strip())
-        print()
+    #for index, ingredients_list in allIngredients.items() : # index = 0, ingredients_list in 
+        #print(f"Recipe {index} ingredients:")
+        #for ingredient in ingredients_list:
+            #print(ingredient.strip())
+        #print()
 
     ingredientInput = "flour, chicken" # ingredients
     print(ingredientInput)
 
-    ingredientsList = [ingredientInput.split(",")] # list of ingredients that were input
+    ingredientsList = ingredientInput.split(",") # list of ingredients that were input
+    print("YAY")
     print(ingredientsList)
     
+    #iterate ingredientInput + ingredient list
+
+    #count variable to keep track of matches
+    # max variable to keep track of max matches
+    # index variable to keep track of index with max matches
+    count = 0
+    max = 0
+    index = 0
+
+    #ALL INGREDIENTS IN THE DATABASE
+    print("first element in all ingredients")
+    print(allIngredients[0])
+    print("first ingredient in recipe")
+    print(allIngredients[0][0])
+
+    
+    for i in range(len(allIngredients)):
+        #LIST OF INGREDIENTS WITHIN A RECIPE
+        for j in range(len(allIngredients[i])):
+             #COMPARE INPUT LIST AND allIngredients[i]
+             for k in range(len(ingredientsList)):
+                 if(ingredientsList[k] in allIngredients[i][j]):
+                     print("ingredlist K")
+                     print(ingredientsList[k])
+                     print("index", i)
+                     count += 1
+        if(count > max):
+            max = count
+            index = i
+    #print(ingredientsList)
+    #display the info for the recipe that matches (use index variable in the nested for loops above)
+            
+
+
+    
+
+    #display the recipe at index
+    
+        #print(allIngredients[index])
 
     page = """
     """
