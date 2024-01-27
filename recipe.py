dee0327b4ae487b716358780b8e8f082fd59b2d5
bpalanka <bela.palankar@gmail.com>
2024-01-26 23:30:55 -0500

    for loop

--- recipe.py
+++ recipe.py
@@ -2,5 +2,10 @@
 import pandas as pd
 import taipy
 
+import csv
 print ("hello world")
 
+with open("./ingredients.csv", 'r') as file:
+  csvread = csv.reader(file)
+  for r in csvread:
+    print(r)
